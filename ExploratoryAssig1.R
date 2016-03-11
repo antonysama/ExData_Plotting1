7f43c14df9b5262948be7bea3e56fb02bfd2b845
Antony Samarawickrema <antonysama@gmail.com>
2016-03-10 23:20:08 -0700

    Update ExploratoryAssig1.R

--- ExploratoryAssig1.R
+++ ExploratoryAssig1.R
@@ -1,95 +1,96 @@
-install.packages("lubridate")
-install.packages("reshape2")
-#fIndout cases
-tab5rows <- read.table("power2.txt", header = TRUE, sep = ";" , nrows = 5)
-A<-sapply(tab5rows, class)
-V<-names(tab5rows)
-
-#read table and subest from 1/2/2007 untill end of 2/2/2007
-dfA<-read.table("power2.txt", header = F, sep = ";" , skip=66636, 
-                nrows = 2881, colClasses = c(A) )#maybe add na.strings="?"
-#pass names
-names(dfA)<-names(tab5rows)
-
-#paste date & time
-l<-paste(dfA$Date, dfA$Time)
-#parse date & time thru lubridate
-library(lubridate)
-nt<-dmy_hms(l)
-#cbind to dfA
-dfA<-cbind(dfA, nt)
-#[alternative]:dataset$DateTime <- strptime(paste(dataset$Date, dataset$Time), "%d/%m/%Y %H:%M:%S")
-
-
-#create png template
-png(filename="plot1.png", width=480, height=480)
-
-#PLOT#1
-png(filename="plot1.png", width=480, height=480) or
-png("Plot1.png", width = 480, height = 480, units = "px", bg = "white")
-par(mar= c(4, 4, 2, 1))
-
-hist(dfA[,3], col = "red",breaks = 12, main = "GlobalActivePower", 
-     xlab = "GlobalActivePower", ylab="Frequency")
+#Q1Have total emissions from PM2.5 decreased from 1999 to 2008? 
+#Using the base plottin
+#Downloaded and extracted two zipped files in working directory. 
+#Reading the two files and subsetting Emissions & year
+Code<-readRDS("Code2.rds")
+pm25<-readRDS("PM25(2).rds")
+pm25<-pm25[,c(4,6)]
+#dplyr to sum emissions by year 
+library("dplyr")
+e<-pm25%>%
+  group_by(year)%>%
+  summarise_each(funs(sum, mean))
+#base plot & save
+with(e, plot(year, sum, type="b", main="US total emissions from PM2.5"))
+dev.copy(png,'plot1.png')
 dev.off()
 
-## Alternative:Save the plot to the appropriate file.
+#Q2 Have total emissions from PM2.5 decreased in the  Baltimore City?
+#read & subset Baltimire emisions by year
+pm25<-readRDS("PM25(2).rds")
+q2<-pm25[pm25$fips=="24510",c(4,6)]
+#dplyr to sum emissions by year
+library("dplyr")
+  q2<-q2%>%
+    group_by(year)%>%
+    summarise_each(funs(sum))
+#base plot & save
+with(q2, plot(year, Emissions,type="b", main="Total PM2.5 in Baltimore City"))
 dev.copy(png,'plot2.png')
-
-#PLOT2
-png(filename="plot2.png", width=480, height=480)
-with(dfA, plot(nt, Global_active_power, type = 'l'))
 dev.off()
 
-#PLOT3 
-#subset what we need
-dfB<-dfA[,c(7,8,9,10)]
-#melt it
-library("reshape2")
-m<-melt(dfB, id="nt")
-names(m)[3]<-"Sub_metering"
-#plot nt v rest, lines on m1-3
-with(m, plot(nt, Sub_metering, xlab="", ylab="Energy sub metering", type = 'l'))
-with(subset(m,variable=="Sub_metering_1"), lines(nt, Sub_metering, col="black"))
-with(subset(m,variable=="Sub_metering_2"), lines(nt, Sub_metering, col="red"))
-with(subset(m,variable=="Sub_metering_3"), lines(nt, Sub_metering, col="blue"))
-legend("topright", pch=1, col=c("black", "red","blue"),
-       legend=c("Sub_metering1", "Sub_metering2", "Sub_metering3"))
-dev.off()
-#alternative:
-with(dataset, plot(DateTime, Sub_metering_1, xlab="", ylab="Energy sub metering", type='l')) 
-with(dataset, lines(DateTime, Sub_metering_2, col='red')) 
-with(dataset, lines(DateTime, Sub_metering_3, col='blue'))
-legend("topright", col=c('black', 'red', 'blue'), legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lty=1)
+#Q3 Which type of emissions has decreased for Baltimore city?
+#read and subset Baltimore emissions by year by type
+q3<-pm25[pm25$fips=="24510",c(4:6)]
+#dplyr to sum emissions by type & year 
+library("dplyr")
+q3<-q3%>%
+  group_by(type, year)%>%
+  summarise_each(funs(sum))
+library("ggplot2")
+qplot(year, Emissions, data=q3, colour=type, geom = c("point", "smooth"),
+      main="Which Source of emissions have decreased for Baltimore city")
+dev.copy(png,'plot3.png')
+dev.off()     
 
+#Q4. How have emissions from coal combustion changed across the US 
+#subset df contating Comb & Coal
+Code2<-Code[grep("Comb(.*)Coal", Code$EI.Sector),]
+SCC2<-Code2[,1]
+#after merging check that no. observations are <pm25
+m2<-merge(Code2,pm25, by.x="SCC", by.y="SCC")
+#get what we need
+m3<-m2[,c(18,20)]
+#dplyr by year, Emissions
+library("dplyr")
+m3<-m3%>%
+  group_by(year)%>%
+  summarise_each(funs(sum))
+#ggplot by year
+library("ggplot2")
+qplot(year, Emissions, data=m3, geom = c("point", "smooth"),
+      main="How have emissions from coal combustion changed across the US")
+dev.copy(png,'plot4.png')
+dev.off()  
 
-#PLOT4
-#format plotter
-par(mfrow = c(2, 2))
-#plot 1-4
-with(dfA, {
-  plot(nt, Global_active_power)
-  lines(nt,Global_active_power)
-})
-
-#2
-with(dfA, plot(nt, Voltage, xlab="datetime", ylab="Voltage", type='l')
-
-#3
-#subset what we need
-dfB<-dfA[,c(7,8,9,10)]
-#melt it
-library("reshape2")
-m<-melt(dfB, id="nt")
-names(m)[3]<-"Sub_metering"
-#plot nt v rest, lines on m1-3
-with(m, plot(nt, Sub_metering, xlab="", ylab="Energy sub metering", type = 'l'))
-with(subset(m,variable=="Sub_metering_1"), lines(nt, Sub_metering, col="black"))
-with(subset(m,variable=="Sub_metering_2"), lines(nt, Sub_metering, col="red"))
-with(subset(m,variable=="Sub_metering_3"), lines(nt, Sub_metering, col="blue"))
-legend("topright", pch=1, col=c("black", "red","blue"),
-       legend=c("Sub_metering1", "Sub_metering2", "Sub_metering3"))
-#4
-with(dfA, plot(nt, Global_reactive_power, xlab="datetime", 
-          ylab="Global_reactive_power", type='l'))
-dev.off()
+#Q5 How have emissions from motor vehicle sources changed in Baltimore City
+#subset Baltimore & On-road
+q5<-pm25[pm25$fips=="24510" & pm25$type=="ON-ROAD",]
+#take yEAR and emmissions
+q5<-q5[,c(4,6)]
+#by year, Emissions
+library("dplyr")
+q5<-q5%>%
+  group_by(year)%>%
+  summarise_each(funs(sum))
+#ggplot by year
+library("ggplot2")
+qplot(year, Emissions, data=q5,geom = c("point", "smooth"),
+  main="How have emissions from motor vehicle sources changed in Baltimore City")
+dev.copy(png,'plot5.png')
+dev.off() 
+     
+#Q6 Baltimore & LA county, on-road
+q6<-pm25[pm25$fips==c("24510","06037") & pm25$type=="ON-ROAD",]
+#take yEAR , emmissions & fips
+q6<-q6[,c(1,4,6)]
+#by fips, fyear, Emissions
+q6<-q6%>%
+  group_by(fips,year)%>%
+  summarise_each(funs(sum))
+#ggplot by year
+library("ggplot2")
+qplot(year, Emissions, data=q6, facets=.~fips, geom = c("point", "smooth"),
+      main="Onroad emissions  from Baltimore (#24510) & LA county(#06037)")
+dev.copy(png,'plot6.png')
+dev.off() 
